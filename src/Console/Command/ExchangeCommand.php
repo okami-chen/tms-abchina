2144f6b672dcacd22654eaf670a8d598cd049695
陈德华 <chendehua@enmonster.org>
2018-07-26 17:47:46 +0800

    命令行优化

--- src/Console/Command/ExchangeCommand.php
+++ src/Console/Command/ExchangeCommand.php
@@ -13,7 +13,9 @@ class ExchangeCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'tms:abchina:exchange';
+    protected $signature = 'tms:abchina:exchange 
+            {--force= : 强制循环}
+            {--sleep= : 休眠毫秒,默认休眠300毫秒}';
 
     /**
      * The console command description.
@@ -41,11 +43,13 @@ class ExchangeCommand extends Command
      */
     public function handle()
     {
+        
         $now    = config('abchina:exchange');
         if(!$now){
             $this->error('请先配置秒杀');
             return false;
         }
+        
         $data   = json_decode($now, true);
         $name   = $this->cacheKey.$data['actPicId'];
         $ret    = ConfigModel::where(['name'=>$name])->first();
@@ -53,8 +57,24 @@ class ExchangeCommand extends Command
             $this->error('未找到活动详情');
             return true;
         }
+        
         $active = json_decode($ret->value, true);
-        $this->secKill($active, $data);
+        $force  = $this->option('force');
+        $sleep  = $this->option('sleep') ?? 300;
+        
+        if($force){
+            
+            while(true){
+                $this->secKill($active, $data);
+                list($msec, $sec) = explode(' ', microtime());
+                $mtime =  (float)sprintf('%.06f', (floatval($msec) + floatval($sec)));
+                $this->line($mtime);
+                usleep($sleep);
+            }
+        }else{
+            $this->secKill($active, $data);
+        }
+        
     }
     
     protected function secKill($active, $data){
@@ -79,7 +99,32 @@ class ExchangeCommand extends Command
         ];
         $response   = $client->post('/yh-web/customer/giftTokenDraw', $option);
         $result     = json_decode($response->getBody()->getContents(), true);
-        dd($result);
+        $result['title']    = array_get($active, 'yhDetail.ruleName');
+        $result['desc'] = array_get($active, 'yhDetail.ruleDesc');
+
+        $headers    = [
+            '状态','结果','活动','描述'
+        ];
+        $rows   = [
+            array_get($result, 'status'),
+            array_get($result, 'result'),
+            array_get($result, 'title'),
+            array_get($result, 'desc')
+            
+        ];
+        
+//        $this->table($headers, $rows);
+        
+        $this->line('状态 : '.array_get($result, 'status'));
+        $this->line('结果 : '.array_get($result, 'result'));
+        $this->line('活动 : '.array_get($result, 'title'));
+        $this->line('描述 : '.array_get($result, 'desc'));
+        
+        $this->split();
+    }
+    
+    protected function split(){
+        $this->line('----------------------------------------------');
     }
     
 }
