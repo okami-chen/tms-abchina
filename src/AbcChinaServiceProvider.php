22b73f257673664dc5cad3006260c772dcaf0463
陈德华 <chendehua@enmonster.org>
2018-07-26 11:47:56 +0800

    init

---
+++ src/AbcChinaServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OkamiChen\TmsAbchina;
+
+use Illuminate\Support\ServiceProvider;
+
+class AbcChinaServiceProvider extends ServiceProvider
+{
+    
+    /**
+     * @var array
+     */
+    protected $commands = [
+        
+    ];
+    
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+}
